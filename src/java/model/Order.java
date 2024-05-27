f037eb28378f496bfd58302a1aa07d64e1de92f5
NamQD <namqd1403@gmail.com>
2024-05-27 11:37:26 +0700

    Add class Order

---
+++ src/java/model/Order.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Namqd
+ */
+public class Order {
+    private int orderID;
+    private int accountID;
+    private Date orderDate;
+
+    public Order() {
+    }
+
+    public Order(int orderID, int accountID, Date orderDate) {
+        this.orderID = orderID;
+        this.accountID = accountID;
+        this.orderDate = orderDate;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+    
+    
+}
