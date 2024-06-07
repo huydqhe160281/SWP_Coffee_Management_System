cbf4039e28dd3d38e987680694d9b3835c96f768
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-06-07 11:20:44 +0700

    feat: update general servlet to update

--- src/java/generalController/UpdateGeneralServlet.java
+++ src/java/generalController/UpdateGeneralServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package generalController;
 
 import dal.GeneralDAO;
@@ -19,34 +18,37 @@ import model.General;
  * @author ADMIN
  */
 public class UpdateGeneralServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet UpdateGeneralServlet</title>");  
+            out.println("<title>Servlet UpdateGeneralServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet UpdateGeneralServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet UpdateGeneralServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -60,16 +62,16 @@ public class UpdateGeneralServlet extends HttpServlet {
         GeneralDAO generalDao = new GeneralDAO();
         try {
             General general = generalDao.getLastGeneral();
-            // Convert the General object to JSON
             request.setAttribute("generalInfo", general);
             request.getRequestDispatcher("updateGeneral.jsp").forward(request, response);
         } catch (Exception e) {
             System.out.println(e);
         }
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -77,12 +79,32 @@ public class UpdateGeneralServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException {
+        int generalID = Integer.parseInt(request.getParameter("generalID"));
+        String email = request.getParameter("email");
+        int phone = Integer.parseInt(request.getParameter("phone"));
+        String nameApp = request.getParameter("nameApp");
+        String address = request.getParameter("address");
+        String logoImage = request.getParameter("logoImage");
+        String fivicoImage = request.getParameter("fivicoImage");
+        GeneralDAO generalDao = new GeneralDAO();
+
+        // Tạo đối tượng General
+        General general = new General(generalID, email, phone, nameApp, address, logoImage, fivicoImage);
+
+        // Cập nhật thông tin
+        boolean isUpdated = generalDao.updateGeneral(general, generalID);
+
+        // Điều hướng tới trang kết quả
+        if (isUpdated) {
+            request.setAttribute("generalInfo", generalDao.getLastGeneral());
+            request.getRequestDispatcher("general.jsp").forward(request, response);
+        }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
