42e9f3d1f495b934209be9544c3e74822f705546
NamQD <namqd1403@gmail.com>
2024-06-11 09:08:57 +0700

    done view detail Supplier

---
+++ src/java/SupplierController/ViewSupplierServlet.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package SupplierController;
+
+import dal.SupplierDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Supplier;
+
+/**
+ *
+ * @author Namqd
+ */
+public class ViewSupplierServlet extends HttpServlet {
+   
+    private final SupplierDAO supplierDAO;
+
+    public ViewSupplierServlet() {
+        this.supplierDAO = new SupplierDAO();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int supplierID = Integer.parseInt(request.getParameter("supplierID"));
+        Supplier supplier = supplierDAO.getSupplierById(supplierID);
+        if (supplier != null) {
+            request.setAttribute("supplier", supplier);
+            request.getRequestDispatcher("/supplierDetail.jsp").forward(request, response);
+        } else {
+            request.setAttribute("error", "Supplier not found.");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
