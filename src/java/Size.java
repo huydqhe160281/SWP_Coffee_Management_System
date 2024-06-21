1308ae422122ca1213f73ec24d9e926cbba1dd41
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-06-21 12:19:48 +0700

    feat: add size model

---
+++ src/java/Size.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Size {
+
+    private int sizeID;
+    private String type;
+
+    public Size() {
+    }
+
+    public Size(int sizeID, String type) {
+        this.sizeID = sizeID;
+        this.type = type;
+    }
+
+    public int getSizeID() {
+        return sizeID;
+    }
+
+    public void setSizeID(int sizeID) {
+        this.sizeID = sizeID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Size{" + "sizeID=" + sizeID + ", type=" + type + '}';
+    }
+}
